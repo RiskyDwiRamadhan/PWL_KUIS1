62b962f785b7fcd59b69e1e77c5bd0b6aa379499
Dimas <midxinnn@gmail.com>
2021-03-13 05:57:03 +0700

    Membuat seeder Pegawai dan Supplier

--- database/seeders/PegawaiSeeder.php
+++ database/seeders/PegawaiSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class PegawaiSeeder extends Seeder
 {
@@ -13,6 +14,73 @@ class PegawaiSeeder extends Seeder
      */
     public function run()
     {
-        //
+        DB::table('pegawais')->insert([
+            [
+                'image' => '/template/img/profile.png',
+                'nama' => 'John',
+                'tanggal' => now(),
+                'komentar' => 'Ini adalah pegawai kami'
+            ],
+            [
+                'image' => '/template/img/profile.png',
+                'nama' => 'Dimas',
+                'tanggal' => now(),
+                'komentar' => 'Ini adalah pegawai kami'
+            ],
+            [
+                'image' => '/template/img/profile.png',
+                'nama' => 'Udin',
+                'tanggal' => now(),
+                'komentar' => 'Ini adalah pegawai kami'
+            ],
+            [
+                'image' => '/template/img/profile.png',
+                'nama' => 'Mad',
+                'tanggal' => now(),
+                'komentar' => 'Ini adalah pegawai kami'
+            ],
+            [
+                'image' => '/template/img/profile.png',
+                'nama' => 'Rina',
+                'tanggal' => now(),
+                'komentar' => 'Ini adalah pegawai kami'
+            ],
+            [
+                'image' => '/template/img/profile.png',
+                'nama' => 'Mas',
+                'tanggal' => now(),
+                'komentar' => 'Ini adalah pegawai kami'
+            ],
+            [
+                'image' => '/template/img/profile.png',
+                'nama' => 'Abdul',
+                'tanggal' => now(),
+                'komentar' => 'Ini adalah pegawai kami'
+            ],
+            [
+                'image' => '/template/img/profile.png',
+                'nama' => 'Anton',
+                'tanggal' => now(),
+                'komentar' => 'Ini adalah pegawai kami'
+            ],
+            [
+                'image' => '/template/img/profile.png',
+                'nama' => 'Bawan',
+                'tanggal' => now(),
+                'komentar' => 'Ini adalah pegawai kami'
+            ],
+            [
+                'image' => '/template/img/profile.png',
+                'nama' => 'Budi',
+                'tanggal' => now(),
+                'komentar' => 'Ini adalah pegawai kami'
+            ],
+            [
+                'image' => '/template/img/profile.png',
+                'nama' => 'Sugi',
+                'tanggal' => now(),
+                'komentar' => 'Ini adalah pegawai kami'
+            ]
+        ]);
     }
 }
--- database/seeders/SupplierSeeder.php
+++ database/seeders/SupplierSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class SupplierSeeder extends Seeder
 {
@@ -13,6 +14,73 @@ class SupplierSeeder extends Seeder
      */
     public function run()
     {
-        //
+        DB::table('suppliers')->insert([
+            [
+                'image' => '/template/img/profile.png',
+                'nama' => 'Abdulloh',
+                'tanggal' => now(),
+                'komentar' => 'Ini adalah supplier kami'
+            ],
+            [
+                'image' => '/template/img/profile.png',
+                'nama' => 'Awang',
+                'tanggal' => now(),
+                'komentar' => 'Ini adalah supplier kami'
+            ],
+            [
+                'image' => '/template/img/profile.png',
+                'nama' => 'Aqil',
+                'tanggal' => now(),
+                'komentar' => 'Ini adalah supplier kami'
+            ],
+            [
+                'image' => '/template/img/profile.png',
+                'nama' => 'Lif',
+                'tanggal' => now(),
+                'komentar' => 'Ini adalah supplier kami'
+            ],
+            [
+                'image' => '/template/img/profile.png',
+                'nama' => 'Syukriansah',
+                'tanggal' => now(),
+                'komentar' => 'Ini adalah supplier kami'
+            ],
+            [
+                'image' => '/template/img/profile.png',
+                'nama' => 'Mid',
+                'tanggal' => now(),
+                'komentar' => 'Ini adalah supplier kami'
+            ],
+            [
+                'image' => '/template/img/profile.png',
+                'nama' => 'Abdul',
+                'tanggal' => now(),
+                'komentar' => 'Ini adalah supplier kami'
+            ],
+            [
+                'image' => '/template/img/profile.png',
+                'nama' => 'Ham',
+                'tanggal' => now(),
+                'komentar' => 'Ini adalah supplier kami'
+            ],
+            [
+                'image' => '/template/img/profile.png',
+                'nama' => 'Awan',
+                'tanggal' => now(),
+                'komentar' => 'Ini adalah supplier kami'
+            ],
+            [
+                'image' => '/template/img/profile.png',
+                'nama' => 'Toro',
+                'tanggal' => now(),
+                'komentar' => 'Ini adalah supplier kami'
+            ],
+            [
+                'image' => '/template/img/profile.png',
+                'nama' => 'Ibrahim',
+                'tanggal' => now(),
+                'komentar' => 'Ini adalah supplier kami'
+            ]
+        ]);
     }
 }
