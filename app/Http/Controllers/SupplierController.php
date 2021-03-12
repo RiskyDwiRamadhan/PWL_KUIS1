ef669c9fa4d4cbf05776da7fba8546db26268538
Dimas <midxinnn@gmail.com>
2021-03-13 06:11:45 +0700

    Membuat Controller pegawai dan supplier

--- app/Http/Controllers/SupplierController.php
+++ app/Http/Controllers/SupplierController.php
@@ -2,9 +2,14 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Supplier;
 use Illuminate\Http\Request;
 
 class SupplierController extends Controller
 {
-    //
+    public function index()
+    {
+        $supplier = Supplier::all();
+        return view('supplier', ['data' => $supplier], ['title' => 'Supplier']);
+    }
 }
