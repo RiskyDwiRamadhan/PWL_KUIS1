d1e2ba0cc777a37d2c94072dcd0f0a5f533c5a20
Dimas <midxinnn@gmail.com>
2021-03-13 05:39:54 +0700

    Create Models pegawai dan supplier

---
+++ database/migrations/2021_03_12_223818_create_suppliers_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateSuppliersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('suppliers', function (Blueprint $table) {
+            $table->id();
+            $table->string('image', 100)->index()->nullable();
+            $table->string('nama', 100)->index()->nullable();
+            $table->date('tanggal')->index()->nullable();
+            $table->string('komentar', 255)->index()->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('suppliers');
+    }
+}
